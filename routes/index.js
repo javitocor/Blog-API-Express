145f6073d32943bd07534bbaeff4aa7aa4b9e404
javitocor <56809963+javitocor@users.noreply.github.com>
2020-10-15 13:56:52 +0200

    implement index routes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,17 @@ var index_controller = require('../controllers/indexController');
 
 
 router.get('/', function(req, res) {
-  res.redirect('/posts');
+  if(req.user){
+    res.redirect('/posts');
+  } else {
+    res.redirect('/log-in');
+  }  
 });
+
+router.post("/sign-up", index_controller.signup_post);
+//Post login page
+router.post("/log-in", index_controller.login_post);
+//Get logout
+router.get("/log-out", index_controller.logout_get);
 
 module.exports = router;
